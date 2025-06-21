c656ec73e96b0043107ec8a001c0973e6e7e6266
akfahad47 <hello.akfahad@gmail.com>
2025-06-21 19:45:48 -0400

    Componentize with ViewComponent and PartialView

--- WebApp/Pages/Employees/Index.cshtml.cs
+++ WebApp/Pages/Employees/Index.cshtml.cs
@@ -1,12 +1,15 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApp.Models;
 
 namespace WebApp.Pages.Employees
 {
     public class IndexModel : PageModel
     {
+        public List<Employee>? Employees { get; set; }
         public void OnGet()
         {
+            this.Employees = EmployeeRepository.GetEmployees();
         }
     }
 }
---
+++ WebApp/Pages/Shared/Components/EmployeeList/EmployeeListViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Pages.Shared.Components.EmployeeList
+{
+    public class EmployeeListViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(string? filter)
+        {
+            return View(EmployeeRepository.GetEmployees(filter));
+        }
+    }
+}
