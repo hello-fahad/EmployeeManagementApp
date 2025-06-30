58b8fa3a27a8ad1c6bcb854bf716570e5ac8df82
akfahad47 <hello.akfahad@gmail.com>
2025-06-29 22:53:52 -0400

    Add project files.

---
+++ WebApp/Model/IDepartmentsApiRepository.cs
@@ -0,0 +1,15 @@
+﻿using WebApp.Models;
+
+namespace WebApp.Model
+{
+    public interface IDepartmentsApiRepository
+    {
+
+        Task<Department?> GetDepartmentByIdAsync(int id);
+        Task AddDepartmentAsync(Department? department);
+        Task<bool> DeleteDepartmentAsync(Department? department);
+        Task<bool> DepartmentExistsAsync(int departmentId);
+        Task<List<Department>> GetDepartmentsAsync(string? filter = null);
+        Task<bool> UpdateDepartmentAsync(Department? department);
+    }
+}
